ca8d3e0f684b3d641909758bca82df81a56c3afb
odinakazy <odinakasolomon4@gmail.com>
2023-04-04 22:04:30 +0100

    team sectiob updated

--- src/Team/Team.jsx
+++ src/Team/Team.jsx
@@ -1,5 +1,24 @@
 import React from "react";
-
+import "./Team.css";
+import Team1 from "../assets/Team1.png";
+import Team2 from "../assets/Team2.png";
+import Team3 from "../assets/Team3.png";
 export default function Team() {
-  return <div>Team</div>;
+  return (
+    <div className="team_container">
+      <h6>The Transporter</h6>
+      <h3>Meet Expert Team</h3>
+      <div className="team_image">
+        <div className="pic">
+          <img src={Team1} alt="logo" />
+        </div>
+        <div className="pic">
+          <img src={Team2} alt="logo" />
+        </div>
+        <div className="pic">
+          <img src={Team3} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }
